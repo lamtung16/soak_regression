e02bf2714906372609bcbb1f1b032081f0f43644
lamtung16 <nguyenlamtung10@gmail.com>
2025-11-21 14:03:52 -0700

    update visualization and mae using median

--- soak_low_level.py
+++ soak_low_level.py
@@ -1,17 +1,18 @@
 import numpy as np
 from sklearn.model_selection import KFold
-from sklearn.linear_model import LinearRegression, RidgeCV
+from sklearn.linear_model import RidgeCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neural_network import MLPRegressor
+from scipy.stats import uniform
 
 
 def evaluate(y_pred, y_test):
-    mse = np.mean((y_test - y_pred) ** 2)
-    mae = np.mean(np.abs(y_test - y_pred))
-    return mse, mae
+    rmse = np.sqrt(np.mean((y_test - y_pred) ** 2))
+    mae = np.median(np.abs(y_test - y_pred))
+    return rmse, mae
 
 
 def featureless_model(X_train, y_train, X_test, y_test):
@@ -21,7 +22,10 @@ def featureless_model(X_train, y_train, X_test, y_test):
 
 
 def linear_model(X_train, y_train, X_test, y_test):
-    model = LinearRegression()
+    model = Pipeline([
+        ('scaler', StandardScaler()),
+        ('ridge', RidgeCV(alphas=np.logspace(-2, 2, 10), cv=4))
+    ])
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     return evaluate(y_pred, y_test)
@@ -47,10 +51,27 @@ def treeCV_model(X_train, y_train, X_test, y_test):
 
 
 def mlpCV_model(X_train, y_train, X_test, y_test):
-    param_grid = {'hidden_layer_sizes': [(10,), (20,), (10, 10), (20, 20)]}
-    grid_search = GridSearchCV(MLPRegressor(max_iter=1000, tol=0.1), param_grid, cv=4, scoring='neg_mean_squared_error', n_jobs=-1)
-    grid_search.fit(X_train, y_train)
-    y_pred = grid_search.best_estimator_.predict(X_test)
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('mlp', MLPRegressor(max_iter=10000, early_stopping=True))
+    ])
+    param_dist = {
+        'mlp__hidden_layer_sizes': [(20,), (20, 20), (80), (80, 80)],
+        'mlp__activation': ['relu', 'tanh'],
+        'mlp__solver': ['adam', 'lbfgs'],
+        'mlp__alpha': uniform(0.001, 10),
+    }
+    random_search = RandomizedSearchCV(
+        pipeline,
+        param_distributions=param_dist,
+        n_iter=40,
+        cv=4,
+        scoring='neg_mean_squared_error',
+        n_jobs=-1
+    )
+    random_search.fit(X_train, y_train)
+    best_model = random_search.best_estimator_
+    y_pred = best_model.predict(X_test)
     return evaluate(y_pred, y_test)
 
 
@@ -63,7 +84,7 @@ all_models = {
 }
 
 
-class SOAK:
+class SOAKFold:
     def __init__(self, n_splits=5, seed=123):
         self.n_splits = n_splits
         self.seed = seed
@@ -85,35 +106,33 @@ class SOAK:
                     splits.append([subset_value, category, X_train, y_train, X[same_idx[test_idx]], y[same_idx[test_idx]]])
         return splits
     
-    @staticmethod
-    def downsample_majority(X, y, subset_vec, seed=123):
-        np.random.seed(seed)
-        unique_subsets, counts = np.unique(subset_vec, return_counts=True)
-        n_min = counts.min()
-        indices_to_keep = []
-        for subset in unique_subsets:
-            subset_indices = np.where(subset_vec == subset)[0]
-            chosen_indices = np.random.choice(subset_indices, size=n_min, replace=False)
-            indices_to_keep.extend(chosen_indices)
-        indices_to_keep = np.array(indices_to_keep)
-        return X[indices_to_keep], y[indices_to_keep], subset_vec[indices_to_keep]
+    # @staticmethod
+    # def downsample_majority(X, y, subset_vec, seed=123):
+    #     np.random.seed(seed)
+    #     unique_subsets, counts = np.unique(subset_vec, return_counts=True)
+    #     n_min = counts.min()
+    #     indices_to_keep = []
+    #     for subset in unique_subsets:
+    #         subset_indices = np.where(subset_vec == subset)[0]
+    #         chosen_indices = np.random.choice(subset_indices, size=n_min, replace=False)
+    #         indices_to_keep.extend(chosen_indices)
+    #     indices_to_keep = np.array(indices_to_keep)
+    #     return X[indices_to_keep], y[indices_to_keep], subset_vec[indices_to_keep]
 
     @staticmethod
     def model_eval(X_train, y_train, X_test, y_test, model='featureless'):
-        mse, mae = all_models[model](X_train, y_train, X_test, y_test)
-        return mse, mae
+        return all_models[model](X_train, y_train, X_test, y_test)
     
     @staticmethod
-    def plot_metrics(df, subset_value, subset_vec=None, metric='mse', figsize=(5, 3)):
+    def plot_metrics(df, subset_value, subset_vec=None, metric='rmse', figsize=(5, 3)):
         import matplotlib.pyplot as plt
 
         observation_info = "" if subset_vec is None else f"(n = {np.sum(subset_vec == subset_value)})"
 
         df = df[df['subset'] == subset_value].copy()
-        df.loc[:, f'log_{metric}'] = np.log10(df[metric])
         df = df.groupby(['subset', 'category', 'model']).agg(
-            avg=(f'log_{metric}', 'mean'),
-            sd=(f'log_{metric}', 'std'),
+            avg=(f'{metric}', 'mean'),
+            sd=(f'{metric}', 'std'),
         ).reset_index()
         fig, axes = plt.subplots(nrows=3, ncols=1, figsize=figsize, sharex=True)
         models = df['model'].unique()
@@ -125,9 +144,12 @@ class SOAK:
             ax.set_yticklabels([])
             category_data = df[df['category'] == category]
             for k, model in enumerate(models):
-                model_data = category_data[category_data['model'] == model]
-                ax.errorbar(model_data['avg'].values[0], k, xerr=model_data['sd'].values[0], fmt='o', label=model)
-        ax.set_xlabel(f'log({metric.upper()})')
+                row = category_data[category_data['model'] == model]
+                _x = row['avg'].values[0]
+                _xerr = row['sd'].values[0]
+                ax.errorbar(_x, k, xerr=_xerr, fmt='o', label=model)
+                ax.text(_x, k, f"{_x:.3f}±{_xerr:.3f}", ha='center', va='bottom')
+        ax.set_xlabel(f'{metric.upper()}')
         handles, labels = axes[0].get_legend_handles_labels()
         fig.legend(handles[::-1], labels[::-1], loc='upper right', fontsize=9)
         fig.suptitle(f'Subset: {subset_value} {observation_info}', fontsize=10, fontweight='bold')
